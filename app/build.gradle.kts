ca8d3e4c75adba340f7875b0a80af8fbf70c3a72
Leilani-K <leilani.karanja@gmail.com>
2025-08-07 15:06:35 +0300

    exectuable test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,9 +16,9 @@ android {
     defaultConfig {
         applicationId = "com.example.gettingstartedwithjetpackcompose"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
